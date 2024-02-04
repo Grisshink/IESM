6285a83d1a0faef29bf9fe31c1885985e88e9505
Griss <grisshink@mail.ru>
2024-02-04 17:43:20 +0300

    Initial commit

---
+++ cloud.py
@@ -0,0 +1,201 @@
+import requests
+import json
+import websocket
+import hashlib
+import random
+import time
+
+from zlib import crc32
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+
+headers = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36',
+    "x-csrftoken": "a",
+    "x-requested-with": "XMLHttpRequest",
+    "referer": "https://scratch.mit.edu",
+}
+
+url = 'wss://clouddata.scratch.mit.edu'
+PROTOCOL_VERSION = bytes([1])
+
+ENCODING = 'cp1251'
+
+def to_bytes(value: int) -> bytes:
+    out = []
+    while value > 0:
+        out.append(value & 0xFF)
+        value >>= 8
+    return bytes(reversed(out))
+
+#def _login(username: str, password: str) -> str:
+#    return 'Some cookie'
+
+def login(username: str, password: str) -> str:
+    data = json.dumps({
+        "username": username,
+        "password": password,
+    })
+    _headers = headers
+    _headers["Cookie"] = "scratchcsrftoken=a;scratchlanguage=en;"
+
+    res = requests.post(
+        "https://scratch.mit.edu/login/",
+        data=data,
+        headers=_headers,
+    )
+    if 'scratchsessionsid' not in res.cookies: raise ValueError('No session id :(')
+
+    return res.cookies['scratchsessionsid']
+
+class WsClosedError(Exception): pass
+class NoVarError(Exception): pass
+
+class DummyWs:
+    def connect(self, *args, **kwargs):
+        _ = args, kwargs
+        self.messages = ['{"method":"set","project_id":"123","name":"☁ cloud 1","value":"100"}\n',
+                         '{"method":"set","project_id":"123","name":"☁ cloud 2","value":"100"}\n',
+                         '{"method":"set","project_id":"123","name":"☁ cloud 3","value":"100"}\n',]
+        #self.messages = []
+
+    def close(self):
+        pass
+
+    def send(self, data: str):
+        print(f'Sent: {data}')
+        if json.loads(data)['method'] == 'set':
+            self.messages.append(data)
+
+    def recv(self) -> str:
+        time.sleep(0.8)
+        try:
+            return random.choice(self.messages)
+        except:
+            return ''
+
+class Connection:
+    def __init__(self, 
+                 project_id: int, 
+                 username: str, 
+                 session_id: str, 
+                 room_name: str,
+                 encoding: str) -> None:
+        self.project_id = project_id
+        self.username = username
+        self.session_id = session_id
+        self.known_vars: set[str] = set()
+        self.room_name = room_name
+        self.room_hash = hashlib.sha256(self.room_name.encode()).digest()
+        self.encoding = encoding
+
+        self.ws = websocket.WebSocket()
+        self.ws.connect(
+            url,
+            cookie="scratchsessionsid=" + self.session_id + ";",
+            origin="https://scratch.mit.edu",
+            enable_multithread=True,
+        )
+
+        self.send_packet({ "method": "handshake", "user": self.username, "project_id": self.project_id })
+
+    def close(self):
+        if self.ws is None: raise WsClosedError('Websocket already closed!')
+        if len(self.known_vars) > 0: self.set_variable(f'{self.username} вышел'.encode(encoding=self.encoding))
+
+        self.ws.close()
+        self.ws = None
+
+    def send_message(self, message: str):
+        enc_message = f'{self.username}> {message.strip()}'.encode(encoding=self.encoding)
+        self.set_variable(enc_message)
+
+    def set_variable(self, value: bytes):
+        if self.ws is None: raise WsClosedError('Websocket not initialised!')
+        if len(value) > 79: raise NameError('Message too long!')
+        if len(self.known_vars) == 0: raise NoVarError('No known vars!')
+
+        cipher = AES.new(self.room_hash, AES.MODE_CBC)
+
+        enc = cipher.encrypt(pad(value, AES.block_size))
+        iv = bytes(cipher.iv)
+        blob = iv + enc
+        hashval = crc32(blob).to_bytes(4)
+
+        inp = int((PROTOCOL_VERSION + hashval + blob).hex(), 16)
+
+        self.send_packet({
+            "method": "set",
+            "name": random.choice(list(self.known_vars)),
+            "value": str(inp),
+            "user": self.username,
+            "project_id": self.project_id,
+        })
+
+    def send_packet(self, data):
+        if self.ws is None: raise WsClosedError('Websocket not initialised!')
+        if 'value' in data and len(data['value']) > 256:
+            raise ValueError(f'Length of payload too long: {len(data["value"])}/256')
+
+        self.ws.send(json.dumps(data) + "\n")
+
+    def add_cloud_var(self, name: str) -> bool:
+        name = f'☁ {name}'
+        if name in self.known_vars: return False
+
+        self.known_vars.add(name)
+
+        if len(self.known_vars) == 1: self.set_variable(f'{self.username} присоеденился'.encode(encoding=self.encoding))
+        return True
+
+    def recv(self) -> str | None:
+        if self.ws is None: raise WsClosedError('Websocket not initialised')
+        out = None
+
+        r = self.ws.recv()
+        if not isinstance(r, str):
+            print(f'Got binary: {r}')
+            return out
+        
+        split_data = r.split('\n')
+        split_data.pop()
+        for packet in split_data:
+            data = json.loads(packet)
+
+            if data['name'] not in self.known_vars:
+                string = f'Discovered new cloud variable: {data["name"]}\n'
+                if out is None: out = string
+                else: out += string
+                self.known_vars.add(data['name'])
+
+                if len(self.known_vars) == 1: self.set_variable(f'{self.username} присоеденился'.encode(encoding=self.encoding))
+            
+            inp = to_bytes(int(data['value']))
+            if len(inp) < 36: return out
+
+            version = inp[0]
+            if version != PROTOCOL_VERSION[0]: continue
+
+            hashval = inp[1:5]
+            blob = inp[5:]
+            if hashval != crc32(blob).to_bytes(4): return out
+
+            iv = blob[:16]
+            data = blob[16:]
+
+            cipher = AES.new(self.room_hash, AES.MODE_CBC, iv)
+            try:
+                dec = unpad(cipher.decrypt(data), AES.block_size).decode(encoding=self.encoding) + '\n'
+                if out is None: out = dec
+                else: out += dec
+
+                print(f'Packet info:'
+                      f'\n\tVersion: {version}'
+                      f'\n\tChecksum: {hashval.hex()}'
+                      f'\n\tIV: {iv.hex()}'
+                      f'\n\tData: {data.hex()}')
+
+            except ValueError:
+                pass
+
+        return out
---
+++ main.py
@@ -0,0 +1,229 @@
+from tkinter import Menu, Tk, ttk, messagebox, Text, END, Toplevel, TclError, simpledialog, StringVar
+import cloud
+import threading
+import time
+import traceback
+
+win = Tk()
+win.title('IESM-Client')
+pad_frame = ttk.Frame(win, padding=10)
+pad_frame.pack()
+
+class DummyWs:
+    def __init__(self) -> None:
+        self.data_counter = 0
+        self.ws = 'asd'
+
+    def close(self):
+        self.ws = None
+
+    def recv(self) -> str:
+        time.sleep(0.5)
+        self.data_counter += 1
+        return f'data: {self.data_counter}\n'
+
+class ChatGUI:
+    def __init__(self, 
+                 username: str,
+                 password: str,
+                 room_name: str,
+                 project_id: int,
+                 encoding: str) -> None:
+        self.session_id = cloud.login(username, password)
+        self.username = username
+        print('Got session id! Loading websocket...')
+        self.ws = cloud.Connection(project_id, username, self.session_id, room_name, encoding)
+        print('Websocket success! Spawning window...')
+
+        self.win = Toplevel(win)
+        self.win.title(f'{self.username} на {project_id}/{room_name}')
+
+        self.menubar = Menu(self.win)
+        self.win.config(menu=self.menubar)
+
+        self.connection_menu = Menu(self.menubar, tearoff=False)
+        self.connection_menu.add_command(
+            label='Добавить облачную переменную', 
+            command=self.cloud_var_popup,
+        )
+        self.connection_menu.add_command(
+            label='Отключиться', 
+            command=self.close_connection,
+        )
+
+        self.menubar.add_cascade(label='Соединение', 
+                                 menu=self.connection_menu,
+                                 underline=0)
+
+        self.pad_frame = ttk.Frame(self.win, padding=10)
+        self.pad_frame.pack()
+
+        self.form_frame = ttk.Frame(self.pad_frame, borderwidth=2, relief='groove')
+        self.form_frame.pack()
+
+        self.chat_text = Text(self.form_frame, state='disabled', wrap='char')
+        self.chat_text.pack()
+
+        self.autoscroll_value = StringVar(value='1')
+        self.autoscroll = ttk.Checkbutton(self.form_frame, text='Автопрокрутка', variable=self.autoscroll_value)
+        self.autoscroll.pack(anchor='w')
+
+        ttk.Label(self.form_frame, text='Отправить сообщение:').pack(anchor='w')
+
+        self.input_entry = ttk.Entry(self.form_frame)
+        self.input_entry.bind('<Return>', self.enter_press)
+        self.input_entry.pack(fill='x')
+
+        recv = threading.Thread(target=self.recieve_text)
+        recv.start()
+
+    def cloud_var_popup(self):
+        out = simpledialog.askstring('Ввод', 'Введите имя облачной переменной (без ☁)')
+        if out is None or out.strip() == '': return
+        
+        out = out.strip()
+        if self.ws.add_cloud_var(out):
+            self.add_text(f'Успешно добавлена переменная: ☁ {out}\n')
+        else:
+            self.add_text(f'Переменная ☁ {out} уже есть в списке переменных\n')
+
+    def close_connection(self):
+        self.ws.close()
+        self.connection_menu.entryconfigure(0, state='disabled')
+        self.connection_menu.entryconfigure(1, state='disabled')
+
+    def enter_press(self, _):
+        message = self.input_entry.get()
+        full_message = f'{self.username}> {message}'
+        try:
+            self.ws.send_message(message)
+            self.add_text(f'{full_message}\n')
+            self.input_entry.delete(0, END)
+        except cloud.WsClosedError:
+            messagebox.Message(
+                message=f'Соединение с проектом закрыто', 
+                parent=win,
+                icon=messagebox.ERROR,
+            ).show()
+        except NameError:
+            length = len(full_message.encode(encoding=cloud.ENCODING))
+            messagebox.Message(
+                message=f'Слишком длинное сообщение!\nЗанято {length} из 79 байт в пакете', 
+                parent=win,
+                icon=messagebox.ERROR,
+            ).show()
+        except cloud.NoVarError:
+            messagebox.Message(
+                message=f'Список доступных облачных переменных пуст!\nДобавьте одно через меню "Соединение" или измените переменные через сам проект',
+                parent=win,
+                icon=messagebox.ERROR,
+            ).show()
+        except Exception as e:
+            exc = ''.join(traceback.format_exception(type(e), e, e.__traceback__))
+            print(exc)
+            self.add_text(f'{exc}\n')
+
+    def add_text(self, text: str):
+        self.chat_text.configure(state='normal')
+        self.chat_text.insert(END, text)
+
+        self.chat_text.config(state='disabled')
+        if self.autoscroll_value.get() == '1': self.chat_text.see(END)
+
+    def recieve_text(self):
+        while True:
+            try:
+                data = self.ws.recv()
+                if data is None:
+                    self.win.state()
+                    continue
+
+                self.add_text(data)
+            except TclError:
+                print('Tk window is dead, closing websocket...')
+                if self.ws.ws is not None: self.ws.close()
+                break
+            except cloud.WsClosedError:
+                self.add_text('Websocket connection closed\n')
+                break
+            except Exception as e:
+                exc = ''.join(traceback.format_exception(type(e), e, e.__traceback__))
+                print(exc)
+                self.add_text(f'{exc}\n')
+                if self.ws.ws is not None: self.ws.close()
+                break
+
+class LoginScreen:
+    def __init__(self) -> None:
+        ttk.Label(pad_frame, text='IESM клиент', font='bold').pack()
+
+        self.form_frame = ttk.Labelframe(pad_frame, borderwidth=2, relief='groove', text='Вход')
+        self.form_frame.pack()
+
+        self.session_frame = ttk.Labelframe(self.form_frame, borderwidth=2, relief='ridge', text='Сессия')
+        self.session_frame.pack(fill='y', side='left', padx=5)
+
+        ttk.Label(self.session_frame, text="Имя пользователя:").pack(anchor='w')
+        self.username_label = ttk.Entry(self.session_frame)
+        self.username_label.pack()
+        ttk.Label(self.session_frame, text="Пароль:").pack(anchor='w')
+        self.password_label = ttk.Entry(self.session_frame, show='*')
+        self.password_label.pack()
+
+        self.address_frame = ttk.Labelframe(self.form_frame, borderwidth=2, relief='ridge', text='Адрес')
+        self.address_frame.pack(fill='y', side='left', padx=5)
+
+        ttk.Label(self.address_frame, text="ID Проекта:").pack(anchor='w')
+        self.project_id_label = ttk.Entry(self.address_frame)
+        self.project_id_label.pack()
+        ttk.Label(self.address_frame, text="Имя комнаты:").pack(anchor='w')
+        self.room_label = ttk.Entry(self.address_frame)
+        self.room_label.pack()
+
+        ttk.Label(self.form_frame, text='Кодировка:').pack(side='top', anchor='w')
+        self.encoding_combo = ttk.Combobox(self.form_frame, values=(
+            'ascii',
+            'cp866',
+            'cp1125',
+            'cp1251',
+            'latin_1',
+            'iso8859_5',
+            'koi8_r',
+            'koi8_u',
+            'utf_8',
+        ), state='readonly')
+        self.encoding_combo.set('iso8859_5')
+        self.encoding_combo.pack(side='top')
+
+        ttk.Button(self.form_frame, text="Вход", command=self.chat_app).pack(side='bottom')
+
+    def chat_app(self):
+        global current_frame
+
+        username = self.username_label.get().strip()
+        password = self.password_label.get()
+        room_name = self.room_label.get()
+        encoding = self.encoding_combo.get()
+
+        if username == '' or password == '':
+            messagebox.Message(
+                message='Заполните все поля, чтобы продолжить', 
+                parent=win,
+                icon=messagebox.ERROR
+            ).show()
+            return
+
+        try:
+            project_id = int(self.project_id_label.get())
+        except ValueError:
+            messagebox.Message(
+                message='Неверный ID проекта (Не является числом)', 
+                parent=win,
+                icon=messagebox.ERROR
+            ).show()
+            return
+
+        ChatGUI(username, password, room_name, project_id, encoding)
+
+current_frame = LoginScreen()
+win.mainloop()
